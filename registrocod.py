6ba4511ac18fc4b8228dc29020738cd44ee6e905
Pedro130505 <brandao.pedro13@gmail.com>
2025-05-01 15:36:48 -0300

    Delete registrocod.py

--- registrocod.py
+++
@@ -1,6 +0,0 @@
-class Registro:
-    def __init__(self, nome, email, senha):
-        self.nome = nome
-        self.email = email
-        self.senha = senha #jogar infos no banco de daos
-        
